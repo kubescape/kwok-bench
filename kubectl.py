673a2d87f1c1f2ec6efe086059bdad66ac945ae5
Amir Malka <amirm@armosec.io>
2023-05-28 13:49:59 +0300

    update script to support deletion and multiple kinds
    Signed-off-by: Amir Malka <amirm@armosec.io>

--- kubectl.py
+++ kubectl.py
@@ -1,12 +1,24 @@
+import argparse
 import subprocess
 from string import Template
-# open templates from files
-with open('daemonset.yaml') as f:
-    daemonset = Template(f.read())
-with open('node.yaml') as f:
-    node = Template(f.read())
-# run kubectl with the generated yamls
-for i in range(1, 11):
-    subprocess.run(['kubectl', 'apply', '-f', '-'], input=daemonset.substitute(id=i).encode())
-for i in range(1, 110):
-    subprocess.run(['kubectl', 'apply', '-f', '-'], input=node.substitute(id=i).encode())
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Script to apply or delete Kubernetes resources')
+parser.add_argument('action', choices=['apply', 'delete'], help='Action to perform: "apply" or "delete"')
+
+# Supported Kubernetes kinds
+# Each kind should have a corresponding YAML template file
+kinds = ['daemonset', 'node', 'service']
+
+# Add arguments for each kind
+for kind in kinds:
+    parser.add_argument(f'--{kind}', type=int, default=0, help=f'Number of {kind}s to create/delete')
+
+args = parser.parse_args()
+
+# Open templates and run kubectl with the generated YAMLs for each kind
+for kind in kinds:
+    with open(f'{kind}.yaml') as f:
+        template = Template(f.read())
+    for i in range(1, getattr(args, kind) + 1):
+        subprocess.run(['kubectl', args.action, '-f', '-'], input=template.substitute(id=i).encode())
